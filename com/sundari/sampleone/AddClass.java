c01b6faaf9719e4a5e471a5929d4b42f19636683
Sundari <sundarims@hotmail.com>
2016-03-29 17:41:21 -0400

    Just commit

---
+++ com/sundari/sampleone/AddClass.java
@@ -0,0 +1,12 @@
+package Sundari.com.sundari.sampleone;
+
+public class AddClass{
+
+    public static void main(String []args) {
+       System.out.println("Inside the AddClass");
+	int a=2;
+	int b=3;
+	int c = a+b;
+	System.out.println("Value of C:"+c);
+    }
+} 
---
+++ com/sundari/sampletwo/PrintClass.java
@@ -0,0 +1,6 @@
+public class PrintClass{
+
+    public static void main(String []args) {
+       System.out.println("Hello World");
+    }
+} 
